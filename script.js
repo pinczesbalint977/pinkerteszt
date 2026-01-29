abdc1abb9167b0195d8a9ba2077ed8c827f36ccf
pinczesbalint977 <pinczesbalint1997>
2026-01-29 18:57:18 +0100

    verzió 1.0

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
-const track = document.querySelector(".gallery-track");
-const nextBtn = document.querySelector(".right");
-const prevBtn = document.querySelector(".left");
+document.querySelectorAll(".gallery-wrapper").forEach(wrapper => {
 
-nextBtn.addEventListener("click", () => {
-    track.scrollLeft += 300; // kb. egy kép szélessége
-});
+  const track = wrapper.querySelector(".gallery-track");
+  const nextBtn = wrapper.querySelector(".right");
+  const prevBtn = wrapper.querySelector(".left");
 
-prevBtn.addEventListener("click", () => {
-    track.scrollLeft -= 300;
+  const images = track.children;
+  const imgWidth = 270; // 250 + gap
+  let index = 0;
+
+  function isDesktop() {
+    return window.innerWidth > 1070;
+  }
+
+  function update() {
+    track.scrollTo({
+      left: index * imgWidth,
+      behavior: "smooth"
+    });
+  }
+
+  nextBtn.addEventListener("click", () => {
+    if (!isDesktop()) return;
+
+    index++;
+    if (index >= images.length) index = 0;
+    update();
+  });
+
+  prevBtn.addEventListener("click", () => {
+    if (!isDesktop()) return;
+
+    index--;
+    if (index < 0) index = images.length - 1;
+    update();
+  });
+
+  window.addEventListener("resize", () => {
+    if (!isDesktop()) {
+      index = 0;
+      track.scrollTo({ left: 0 });
+    }
+  });
+
 });
 
 
